bc78fe69078c2ff736648a3f8046987607e0a782
Roman Dronov <romandronov@gmail.com>
2021-08-21 11:41:14 +1000

    wrapped transcription into a minimum flask app to be used as a service

--- step_3_transcription_app_service.py
+++ step_3_transcription_app_service.py
@@ -6,34 +6,56 @@ Created on Sat Aug 21 11:10:50 2021
 """
 
 
+from flask import Flask 
+import os
+import threading
 from transformers import Wav2Vec2Tokenizer, Wav2Vec2ForCTC
-from datasets import load_dataset
-import soundfile as sf
 import torch
-import os, glob
+import os
 import librosa
-import soundfile as sf
 import json
+import atexit
+import logging
+
+
+### folder_path = "E:\\project\\raw_data\\eedd150a524d388e5f8bd8bfbd81770b34197a8e57f41fa987507ee3ee5f2e8d"
 
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("res\\models\\wav2vec2-aph-cap")
-model = Wav2Vec2ForCTC.from_pretrained("res\\models\\wav2vec2-aph-cap\checkpoint-1500")
+model = Wav2Vec2ForCTC.from_pretrained("res\\models\\wav2vec2-aph-cap\\checkpoint-3000")
+file_path=''
+
+# wrap code into a minimum flask app and spin out threads for async processing
+app = Flask(__name__)
+    
+@app.route('/<path:filename>', methods=['GET', 'POST']) 
+def index(filename):
+    global model, tokenizer, filepath, t_transcribe
+    _path = os.path.abspath(str(filename))
+    _path = _path.replace('//','/')
+    if _path[-4:]!='.wav':
+        print('Processing ',str(_path))
+        #print('file not supported')
+        return "file not supported"
+    else:
+        print('Processing ',str(_path))
+        filepath = _path
+        t_transcribe = threading.Thread(name='trancription daemon', target=transcribeCall)
+        t_transcribe.setDaemon(True)
+        t_transcribe.start()
+    return 'Transcription tread has been started...'
 
 
-folder_path = "E:\\project\\raw_data\\eedd150a524d388e5f8bd8bfbd81770b34197a8e57f41fa987507ee3ee5f2e8d"
 
-
-
-file_list = os.listdir(folder_path)
-lstWav = [f for f in file_list if f.endswith('.wav')]
-tts_str = ""
-for f in lstWav:
-    _path = _path = os.path.abspath(folder_path +'\\'+f)
+def transcribeCall():
+    logging.debug('Starting transcription thread...')
+    global model, tokenizer, filepath
+    _path = filepath
     metad = dict()
     with open(_path.replace('.wav','.json'), 'r') as jsf:
         metad = json.load(jsf)
     # metad['transcription'] contains transcription
     y, sr = librosa.load(_path)
-    hlf = int(len(y)/2) 
+    hlf = int(len(y)/2)
     yy = [y[:hlf], y[hlf:] ]
     transcr = ""
     for prt in yy:    
@@ -44,15 +66,33 @@ for f in lstWav:
         [tts] = transcription
         transcr = transcr + " " + tts
     both_parts = transcr.lower()
-    tts_str = tts_str + " " + both_parts
     metad['transcription'] = both_parts
     with open(_path.replace('.wav','.json'), 'w') as jsf:
         json.dump(metad, jsf, indent=2)
     print(str(both_parts)+'\n')
+    logging.debug('Completed transcription...')
 
 
-
-
-
+# cleaning
+def close_threads():
+    """
+    stops all running daemon threads at exit
+    """    
+    global t_transcribe    
+    try:
+        t_transcribe.cancel()
+        print('Closed transcription thread...')
+    except:
+        print('Could not close transcription thread...')  
+    
+    
+    
+atexit.register(close_threads)
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port = 5001)
+  
+  
+  
+  
 
 
